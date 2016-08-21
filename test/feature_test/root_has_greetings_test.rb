e5d5d0a663b622c12aa60149bef8c968faafb57f
Raphael Barbo <rsbarbo@gmail.com>
2016-08-21 15:16:36 -0600

    added some more tests

--- test/feature_test/root_has_greetings_test.rb
+++ test/feature_test/root_has_greetings_test.rb
@@ -7,4 +7,24 @@ class GreetingPageTest < FeatureTest
     assert page.has_content?("Welcome to Robot World")
   end
 
+  def test_root_contain_statistics_access
+    visit '/'
+    assert page.has_content?("Statistics")
+  end
+
+  def test_root_contain_all_robots
+    visit '/'
+    assert page.has_content?("All Robots")
+  end
+
+  def test_root_contain_new_robots
+    visit '/'
+    assert page.has_content?("New Robot")
+  end
+
+  def test_root_contain_home_page
+    visit '/'
+    assert page.has_content?("Home Page")
+  end
+
 end
